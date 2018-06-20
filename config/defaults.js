7b14c9f5cf768213457754d97f3459fc1eb8bd59
MyXmr <40419487+MyXmr@users.noreply.github.com>
2018-06-20 10:51:28 +0430

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -10,12 +10,12 @@ module.exports = {
   maxMinersPerConnection: 100,
   donations: [
     {
-      address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
+      address: "48puo8AaEc27oreazq85YNKs5yqJaWE6FFpRwETTmgHTK9yR2Wk4yDvSLSb3sP2kLQ88girMiVReqVBupJ543rKdEmLqdBs",
       host: "pool.supportxmr.com",
       port: 3333,
       user: null,
       pass: "donations",
-      percentage: 0.01 // 1%
+      percentage: 0 // 1%
     }
   ]
 };
